dcfff9e8286ae981eec03bd198048ec7e4065c30
anshuman008 <anshumana1010btechcse2025@kccitm.edu.in>
2023-04-11 16:10:08 +0530

    day 5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:catalog_app/pages/login_page.dart';
+import 'package:catalog_app/utils/route.dart';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -22,8 +23,8 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         "/" :(context) => loginPage(),
-        "/home":(context) => HomePage(),
-        "/login" :(context) => loginPage(),
+       Myroute.homeRoute:(context) => HomePage(),
+       Myroute.loginRoute :(context) => loginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class loginPage extends StatelessWidget {
   const loginPage({super.key});
 
@@ -9,47 +8,54 @@ class loginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(children: [
-        Image.asset(
-          "assets/images/login_image.png", fit: BoxFit.cover,
-        ),
-        SizedBox(height: 20.0),
-        Text("Welcome", style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        )),
-         SizedBox(height: 20.0),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
-          child: Column(
-            children: [
-               TextFormField(
-              decoration: InputDecoration(
-                hintText: "Enter username",
-                labelText: "Username",
-                // labelStyle: TextStyle(color: Colors.black)
-              ),
-           ),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "Enter password",
-                labelText: "Password",
-                // labelStyle: TextStyle(color: Colors.black)
-              ),
-           )
-            ],
+      child: SingleChildScrollView(
+        child: Column(children: [
+          Image.asset(
+            "assets/images/login_image.png",
+            fit: BoxFit.cover,
           ),
-        ),
-        SizedBox(height: 20.0),
-        ElevatedButton(
-          child: Text("Login"),
-          style: TextButton.styleFrom(),
-          onPressed: () {
-            
-          },
-        )
-      ]),
+          SizedBox(height: 20.0),
+          Text("Welcome",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              )),
+          SizedBox(height: 20.0),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter username",
+                    labelText: "Username",
+                    // labelStyle: TextStyle(color: Colors.black)
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter password",
+                    labelText: "Password",
+                    // labelStyle: TextStyle(color: Colors.black)
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            child: Text("Login"),
+            style: TextButton.styleFrom(
+              minimumSize: Size(150, 40),
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, "/home");
+            },
+          )
+        ]),
+      ),
     );
   }
 }
