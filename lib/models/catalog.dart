8aac68d65d398b549dbc7735b570f0f5a07d9713
anshuman008 <anshumana1010btechcse2025@kccitm.edu.in>
2023-04-13 03:31:02 +0530

    created map for maping jason decoded data

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,14 +1,14 @@
-class CatalogModel{
- static final items = [
-  Item(
-      id: 1,
-      name: "iPhone 12 Pro",
-      desc: "Apple iPhone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-];
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "Apple iPhone 12th generation",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+  ];
 }
 
 class Item {
@@ -19,6 +19,31 @@ class Item {
   final String color;
   final String image;
 
-  Item({this.id = 0, this.name = '', this.desc = '', this.price = 0, this.color = '', this.image = ''});
-}
+  Item(
+      {this.id = 0,
+      this.name = '',
+      this.desc = '',
+      this.price = 0,
+      this.color = '',
+      this.image = ''});
 
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'],
+      name: map['name'],
+      desc: map['desc'],
+      price: map['price'],
+      color: map['color'],
+      image: map['image'],
+    );
+  }
+
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "image": image,
+      };
+}
