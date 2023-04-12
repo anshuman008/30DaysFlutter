3d6d2574d303d6a947dcb8dec177c4bee04e1352
anshuman008 <anshumana1010btechcse2025@kccitm.edu.in>
2023-04-13 03:15:03 +0530

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,31 @@
 import 'package:catalog_app/models/catalog.dart';
 import 'package:catalog_app/widgets/drawer.dart';
+import 'dart:convert';
 import 'package:catalog_app/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
-  var thm = "light";
+
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJason =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeDtat = jsonDecode(catalogJason);
+    var productData = decodeDtat["products"];
+    // print(productData);
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class loginPage extends StatefulWidget {
   const loginPage({super.key});
