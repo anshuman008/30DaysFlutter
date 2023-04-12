7f99fd2bed582eadb40fad4074d91cbe4e4a0a1b
anshuman008 <anshumana1010btechcse2025@kccitm.edu.in>
2023-04-13 02:20:10 +0530

    wrap with card widget

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -9,15 +9,17 @@ final Item item;
   const ItemWidget({ Key? key, required this.item }) : assert(item != null), super(key: key);
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-    leading: Image.network(item.image),
-    title: Text(item.name),
-    subtitle: Text(item.desc),
-    trailing: Text("\$${item.price}", style: TextStyle(
-     fontSize: 20.0,
-      color: Colors.deepPurple,
-      fontWeight: FontWeight.bold,
-    ),),
+    return Card(
+      child: ListTile(
+      leading: Image.network(item.image),
+      title: Text(item.name),
+      subtitle: Text(item.desc),
+      trailing: Text("\$${item.price}", style: TextStyle(
+       fontSize: 20.0,
+        color: Colors.deepPurple,
+        fontWeight: FontWeight.bold,
+      ),),
+      ),
     );
   }
 }
