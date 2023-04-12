0bc6ad714d81a725080a131fea6a25d49718972b
anshuman008 <anshumana1010btechcse2025@kccitm.edu.in>
2023-04-13 04:11:55 +0530

    day 14 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,29 +20,35 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
+    await Future.delayed(Duration(seconds: 2));
     final catalogJason =
         await rootBundle.loadString("assets/files/catalog.json");
     final decodeDtat = jsonDecode(catalogJason);
     var productData = decodeDtat["products"];
+    CatalogModel.items = List.from(productData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+        setState(() {});
     // print(productData);
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('catalog App',
             style: Theme.of(context).appBarTheme.titleTextStyle),
       ),
-      body: ListView.builder(
-        itemCount: dummyList.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(
-            item: dummyList[index],
-          );
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ( CatalogModel.items != null && CatalogModel.items.isNotEmpty)?ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) =>
+            ItemWidget(
+              item: CatalogModel.items[index],
+            ),
+        ):Center(child: CircularProgressIndicator(),),
       ),
       drawer: MyDrawer(),
     );
